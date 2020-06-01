1216fedc4e7faba94d326f2143ca3a4be39dcb83
Prathamesh Sawant <prathamesh2901@gmail.com>
2020-05-31 23:24:31 -0400

    Enabled lookaside cache for country

---
+++ cache/country.py
@@ -0,0 +1,28 @@
+#!/usr/local/bin/python3
+from rejson import Client, Path
+
+
+class CountryCache():
+
+
+    def __init__(self, name):
+        self.name = name
+
+    def find_by_country(self):
+        try:
+            rj = Client(
+                host='redis',
+                port=6379,
+                decode_responses=True)
+            return rj.jsonget(self.name)
+        except:
+            return None
+
+
+    def cache(self, obj):
+        rj = Client(
+            host='redis',
+            port=6379,
+            decode_responses=True)
+        rj.jsonset(self.name, Path.rootPath(), obj)
+
